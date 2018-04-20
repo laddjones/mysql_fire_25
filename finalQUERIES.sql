fb35434c4a5fcc8950660a29b925898aea0a9bef
aruiz1997 <36284460+aruiz1997@users.noreply.github.com>
2018-04-20 13:25:19 -0400

    Update finalQUERIES.sql

--- finalQUERIES.sql
+++ finalQUERIES.sql
@@ -12,9 +12,8 @@ from Property join Visit
 where Property.ApprovedBy <> "Null" and Property.ID=Visit.PropertyID
 group by propertyid;
 
-select * from Property where Name = '+Name+' or Street = '+street+' or City= "City" or Zip = '+zip+'
-or size = '+size+' or PropertyType= '+PropertyType+' or IsPublic='+IsPublic+' or IsCommercial='+IsCommercial+'
-or ID='+ID+' or Owner='+Owner+';
+select * from Property where ('+Searchby+' = '+Option+');
+
 
 ####unconfirmed properties
 SELECT Property.Name, Property.Street, Property.City, Property.Zip, Property.Size, Property.PropertyType,
